3bf78a83d708ab104be446cca7f2ab93ff1a909e
DavidT <davidt.info@gmail.com>
2024-12-04 21:17:41 -0300

    Number

---
+++ tipo-number.js
@@ -0,0 +1,11 @@
+const notaPrimeiroBi = 8;
+const notaSegundoBI = 6.3;
+const notaTerceiroBI = -2;
+const notaQuartoBI = Number.parseInt('5');
+
+const total = (notaPrimeiroBi + notaSegundoBI + notaTerceiroBI + notaQuartoBI) / 4;
+
+console.log('a media é ' + total.toFixed(2));
+
+const nome = 'Juliana';
+console.log('meu nome é ' + nome);
